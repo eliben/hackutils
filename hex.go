1bec495ec7cf15262094f6d9f53444af102bd94f
Eli Bendersky <eliben@gmail.com>
2019-09-11 15:57:46 -0700

    Added implementation of KL divergence

--- hex.go
+++ hex.go
@@ -3,6 +3,7 @@ package hackutils
 import (
 	"encoding/hex"
 	"log"
+	"math"
 )
 
 // HexToBytes converts hexadecimal encoded bytes to raw bytes, failing the
@@ -25,7 +26,7 @@ func HexToBytes(h []byte) []byte {
 // result.
 func XorBytes(b1, b2 []byte) []byte {
 	if len(b1) != len(b2) {
-		log.Fatalf("len(b1) == %v != len(b2) == %v", len(b1), len(b2))
+		log.Fatalf("len(b1) == %v, len(b2) == %v", len(b1), len(b2))
 	}
 	out := make([]byte, len(b1))
 	for i := 0; i < len(b1); i++ {
@@ -42,4 +43,25 @@ func XorBytesWithVal(bs []byte, b byte) []byte {
 		out[i] = bs[i] ^ b
 	}
 	return out
+}
+
+// TODO: This and the Xors don't belong in hex.go -- split it up
+
+// KLDivergence computes the Kullback-Leibler divergence between two probability
+// distributions p and q. p and q are expected to have the same length and
+// contain numbers that sum up to 1.0; The KL divergence is close to 0 if the
+// two distributions are deemed "similar" and gets larger the more different
+// they are.
+// See https://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence
+func KLDivergence(p, q []float64) float64 {
+	if len(p) != len(q) {
+		log.Fatalf("len(p) == %v, len(q) == %v", len(p), len(q))
+	}
+	var total float64
+	for i := 0; i < len(p); i++ {
+		if p[i] > 0 {
+			total += p[i] * math.Log(q[i]/p[i])
+		}
+	}
+	return -total
 }
--- hex_test.go
+++ hex_test.go
@@ -44,3 +44,29 @@ func TestXorBytesWithVal(t *testing.T) {
 		t.Errorf("got %v, want %v", result, expected)
 	}
 }
+
+func TestKLDivergence(t *testing.T) {
+	as := []float64{0.33, 0.33, 0.1, 0.23}
+
+	// Intuitively, these are ordered from the "closest to as" to "farthest from
+	// as".
+	bs := []float64{0.32, 0.34, 0.09, 0.24}
+	cs := []float64{0.27, 0.39, 0.08, 0.25}
+	ds := []float64{0.14, 0.51, 0.25, 0.1}
+	es := []float64{0.1, 0.2, 0.3, 0.4}
+
+	divBs := KLDivergence(as, bs)
+	divCs := KLDivergence(as, cs)
+	divDs := KLDivergence(as, ds)
+	divEs := KLDivergence(as, es)
+
+	if divBs > divCs {
+		t.Errorf("got divBs > divCs")
+	}
+	if divCs > divDs {
+		t.Errorf("got divCs > divDs")
+	}
+	if divDs > divEs {
+		t.Errorf("got divDs > divEs")
+	}
+}
