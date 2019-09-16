64ed540fb912f4ea81cc366ac339cd2f872ffb53
Eli Bendersky <eliben@gmail.com>
2019-09-16 14:48:29 -0700

    Add thorough testing for stripping PKCS#7 padding

--- padding.go
+++ padding.go
@@ -2,6 +2,7 @@ package hackutils
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 )
 
@@ -13,4 +14,25 @@ func PKCS7Padding(data []byte, blockLen int) []byte {
 	}
 	padLen := blockLen - len(data)%blockLen
 	return bytes.Repeat([]byte{byte(padLen)}, padLen)
+}
+
+// StripPKCS7Padding strips PKCS#7 padding from data. Returns a slice of the
+// data without padding (likely to alias data). Error is returned when the
+// padding is incorrect.
+func StripPKCS7Padding(data []byte) ([]byte, error) {
+	// Look at the last byte of padding. For N, expect the last N bytes to be
+	// equal to N and strip them.
+	npad := data[len(data)-1]
+	if len(data) <= int(npad) {
+		return nil, fmt.Errorf("padding error: len(data)=%v, npad=%d", len(data), npad)
+	}
+
+	// Check that all padding bytes are correct.
+	for i := 0; i < int(npad); i++ {
+		if data[len(data)-1-i] != npad {
+			return nil, fmt.Errorf("padding error: data[%d]=%d, npad=%d", len(data)-1-i, data[len(data)-1-i], npad)
+		}
+	}
+
+	return data[:len(data)-int(npad)], nil
 }
--- padding_test.go
+++ padding_test.go
@@ -28,3 +28,67 @@ func TestPKCS7Pad(t *testing.T) {
 		})
 	}
 }
+
+func TestStripPKCS7Padding(t *testing.T) {
+	var tests = []struct {
+		data   []byte
+		hasErr bool
+		result []byte
+	}{
+		{[]byte{10, 20, 1}, false, []byte{10, 20}},
+		{[]byte{10, 2, 2}, false, []byte{10}},
+		{[]byte{1, 3, 3, 3}, false, []byte{1}},
+		{[]byte{10, 20, 2}, true, nil},
+		{[]byte{10, 20, 20}, true, nil},
+		{[]byte{10, 20, 4, 4, 4, 4}, false, []byte{10, 20}},
+		{[]byte{1, 2, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16}, false, []byte{1, 2}},
+	}
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%v", tt.data)
+		t.Run(testname, func(t *testing.T) {
+			result, err := StripPKCS7Padding(tt.data)
+			if err != nil {
+				if !tt.hasErr {
+					t.Errorf("err %v, expected to pass", err)
+				} else {
+					return
+				}
+			}
+			if tt.hasErr {
+				// err == nil, so this isn't expected.
+				t.Errorf("expected err, got %v", tt.result)
+			}
+			if bytes.Compare(result, tt.result) != 0 {
+				t.Errorf("got %v, want %v", result, tt.result)
+			}
+		})
+	}
+}
+
+func TestPKCS7Roundtrip(t *testing.T) {
+	// Padding is done to blocks of 16. Generate all blocks of size 1..31
+	// and make sure their padding works round-trip.
+	var tests [][]byte
+	for i := 1; i <= 31; i++ {
+		d := make([]byte, i)
+		for c := 0; c < i; c++ {
+			d[c] = byte(c) + 1
+		}
+		tests = append(tests, d)
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%v", tt)
+		t.Run(testname, func(t *testing.T) {
+			padding := PKCS7Padding(tt, 16)
+			newdata := append(tt, padding...)
+			nopad, err := StripPKCS7Padding(newdata)
+			if err != nil {
+				t.Error(err)
+			}
+			if bytes.Compare(nopad, tt) != 0 {
+				t.Errorf("got %v, want %v", nopad, tt)
+			}
+		})
+	}
+}
